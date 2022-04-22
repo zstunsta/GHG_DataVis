e43266744b77d9e64b585fbb46c14f39e3d89242
zstunsta <91479271+zstunsta@users.noreply.github.com>
2022-04-21 17:41:55 -0700

    Add files via upload

--- zt1.py
+++ zt1.py
@@ -3,14 +3,20 @@
 4/19/2022
 Author: Zach Tunstall
 Purpose: To generate data visualizations, and provide relavent statistics for global greenhouse gas with agricultural data to use in technical interview for WattTime.
+Usage: Ensure all Excel files are in the same folder as this script.  Check comments before function calls to ensure labels are accurate to charts.
 """
 
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sb
 import colorcet as cc
+import random 
+import os
 
+
+# Change current working directory.
+dir = (os.path.dirname(__file__))
+os.chdir(dir)
 
 # Import excel files and cast to dataframes
 dfCH4_0 = pd.read_excel("ch4_2015-2021.xlsx")
@@ -31,16 +37,18 @@ dfHarv = dfHarv1.iloc[:, 1:]
 
 # Set color palette for plotting.
 clr23 = sb.color_palette(cc.glasbey, n_colors=50)
+cList = ['b','g','c','k','r', 'm']
+cRand = random.choice(cList)
 
 # Transpose dataframes for plotting over time.
 dfEmis_t = dfEmis.set_index('country_fao').T  
 dfCH4_t = dfCH4.set_index('country_name').T
 dfHarv_t = dfHarv.set_index('country_name').T
 
-# Give Name to dataframes.
-dfEmis_t.name = dfEmis.name = 'GHG Emissions'
-dfCH4_t = dfCH4.name = 'Methane Emissions'
-dfHarv_t = dfHarv.name = 'Harvested Area (Ha)'
+# Give title to dataframes.
+dfEmis_t.name = dfEmis.name = 'Rice Cultivation Emissions Estimates'
+dfCH4_t.name = dfCH4.name = 'Methane Emissions'
+dfHarv_t.name = dfHarv.name = 'Harvested Area (Ha)'
 
 # Print data types and details of columns. 
 def print_info(df):
@@ -52,7 +60,9 @@ def describe_data(df):
 
 # Display boxplot of column values. Search for outliers.
 def box_plot(df):
-    return df.boxplot(vert=False, grid=False), plt.title(df.name), plt.show()
+    return (df.boxplot(vert=False, grid=False), 
+    plt.title(df.name),
+    plt.show())
 
 # Display histogram of column values. Search for outliers.
 def hist_plot(df):
@@ -62,15 +72,18 @@ def hist_plot(df):
 def bar_plot(df_t):
     return (df_t.plot.barh(color=clr23), 
     plt.legend(bbox_to_anchor=(1,1), loc="upper left"), 
-    plt.title(df_t.name), 
+    plt.title(df_t.name),     
+    plt.xlabel("Emisions (tons)"),
+    plt.ylabel("Year"),
     plt.show())
 
 # Create Heatmap of percent change between years. Use transposed dataframes.
 def year_change(df_t):
     df_t_h = df_t.pct_change(axis='rows').transpose()
-    return print(df_t_h), sb.heatmap(df_t_h), plt.show()
+    dfname = df_t.name + " % Change"
+    return print(df_t_h), sb.heatmap(df_t_h, cmap='OrRd'), plt.title(dfname), plt.show()
 
-# Create line plot over time, use transposed dataframe.
+# Create line plot over time, use transposed dataframe. 
 def line_plot(df_t, country_name=None):
     """ country_name must be in string format. """
     if country_name == None:
@@ -78,10 +91,43 @@ def line_plot(df_t, country_name=None):
         plt.locator_params(axis='x', nbins=8), 
         plt.legend(bbox_to_anchor=(1,1), loc="upper left"), 
         plt.title(df_t.name),
+        plt.xlabel("Year"),
+        plt.ylabel("Emisions (tons)"),
         plt.show())
     else:
-        return (df_t.plot(color=clr23, x_compat=True, y='{0}'.format(country_name)), 
+        return (df_t.plot(color='r', x_compat=True, y='{0}'.format(country_name)), 
         plt.locator_params(axis='x', nbins=8), 
         plt.legend(bbox_to_anchor=(1,1), loc="upper left"), 
         plt.title(df_t.name),
+        plt.xlabel("Year"),
+        plt.ylabel("Emisions (tons)"),
         plt.show())
+
+
+## Output Rice Harvest Production Charts.
+# Y Labels - lines 69, 88 & 96 Should say "Estimated Hectares Harvasted".
+line_plot(dfHarv_t)
+year_change(dfHarv_t)
+line_plot(dfHarv_t, country_name='India')
+line_plot(dfHarv_t, country_name='China')
+line_plot(dfHarv_t, country_name='Thailand')
+line_plot(dfHarv_t, country_name='Viet Nam')
+
+## Output Methane Emissions Charts.
+# Change Y Labels on lines 69, 88 & 96 to "Emisions (tons)".
+line_plot(dfCH4_t)
+line_plot(dfCH4_t, country_name='India')
+line_plot(dfCH4_t, country_name='China')
+line_plot(dfCH4_t, country_name='Thailand')
+line_plot(dfCH4_t, country_name='Viet Nam')
+year_change(dfCH4_t)
+
+## Output Rice Production Emissions Charts.
+# Change Y Labels on lines 69, 88 & 96 to "Emisions (tons)".
+bar_plot(dfEmis_t)
+line_plot(dfEmis_t)
+line_plot(dfEmis_t, country_name='India')
+line_plot(dfEmis_t, country_name='China, mainland')
+line_plot(dfEmis_t, country_name='Thailand')
+line_plot(dfEmis_t, country_name='Viet Nam')
+year_change(dfEmis_t)
